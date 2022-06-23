c0f1a91fe27021b2c114cfebb2360e303d3fac69
MMeiiii <77165926+MMeiiii@users.noreply.github.com>
2022-06-23 19:24:07 +0800

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main (){
+
+    int a;
+
+    while(cin >> a){
+
+        double b;
+
+        b=a*1.6;
+
+        cout << b;
+
+        cin >> a;
+
+    }
+    return 0;
+}
