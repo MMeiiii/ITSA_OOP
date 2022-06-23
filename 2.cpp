c6dfab0a8bcdd851b5562b542a61000a1948cbdb
MMeiiii <77165926+MMeiiii@users.noreply.github.com>
2022-06-23 19:26:53 +0800

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+
+    int a;
+
+    while (cin >> a)
+    {
+
+        double b;
+
+        b = a * 1.6;
+
+        cout << b;
+
+        cin >> a;
+    }
+    return 0;
+}
